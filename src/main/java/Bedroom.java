97fd8928e459301a77802b6ac9427df6f4781684
Steven Kay <stevendkay@gmail.com>
2019-01-09 11:58:36 +0000

    added tests for guest

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+	private int roomNumber;
+	private int capacity;
+	private float nightlyRate;
+	private String type;
+	private ArrayList<Guest> guests;
+
+	public Bedroom(int roomNumber, int capacity, float nightlyRate, String type) {
+		this.roomNumber = roomNumber;
+		this.capacity = capacity;
+		this.nightlyRate = nightlyRate;
+		this.type = type;
+		this.guests = new ArrayList<>();
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public float getNightlyRate() {
+		return nightlyRate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public ArrayList<Guest> getGuests() {
+		return guests;
+	}
 }
--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+	private Bedroom boudoir;
+	private Bedroom honeymoonSuite;
+
+	@Before
+	public void setUp() {
+		this.boudoir = new Bedroom(new Integer(1),new Integer(2),new Float(200.0),"Double");
+	}
+
+	@Test
+	public void checkRoomHasNumber() {
+		assertEquals(1, boudoir.getRoomNumber());
+	}
+
+	@Test
+	public void checkRoomHasCapacity() {
+		assertEquals(2, boudoir.getCapacity());
+	}
+
+	@Test
+	public void checkRoomHasNightlyRate() {
+		assertEquals(200.0f, boudoir.getNightlyRate(), 0.01);
+	}
+
+	@Test
+	public void checkRoomHasType() {
+		assertEquals("Double", boudoir.getType());
+	}
+
+	@Test
+	public void checkRoomStartsEmpty() {
+		assertEquals(0, boudoir.getGuests().size());
+	}
 }
