75582e713eaf3f645b52546b30b9caffbf6284d1
Rob Wilson <rob.wilson195@gmail.com>
2019-01-09 14:03:03 +0000

    Hotel class started.

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private String name;
+    private ArrayList<Facility> facilities;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<Booking> bookings;
+
+    public Hotel(String name) {
+        this.name = name;
+        this.facilities = new ArrayList<>();
+        this.bedrooms = new ArrayList<>();
+        this.bookings = new ArrayList<>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Facility> getFacilities() {
+        return facilities;
+    }
+
+    public ArrayList<Bedroom> getBedrooms() {
+        return bedrooms;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,50 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HotelTest {
+
+    private Bedroom boudoir;
+    private Bedroom honeymoonSuite;
+    private Guest guest1;
+    private Guest guest2;
+    private ArrayList<Guest> guestList;
+    private Hotel hotel;
+
+
+    @Before
+    public void setUp() {
+        this.boudoir = new Bedroom(new Integer(1),new Integer(2),new Float(200.0),"Double");
+        this.guest1 = new Guest("Steve");
+        this.guest2 = new Guest("Rob:");
+        guestList = new ArrayList<>();
+        guestList.add(guest1);
+        guestList.add(guest2);
+        this.hotel = new Hotel("The Scotsman");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("The Scotsman", hotel.getName());
+    }
+
+    @Test
+    public void hasNoRooms() {
+        assertEquals(0, hotel.getBedrooms().size());
+    }
+
+    @Test
+    public void hasNoFacilities() {
+        assertEquals(0, hotel.getFacilities().size());
+    }
+
+    @Test
+    public void hasNoBookings() {
+        assertEquals(0, hotel.getBookings().size());
+    }
+
+
 }
