960dd42e087612e03114987e9e2a7ca38ca64fbd
Rob Wilson <rob.wilson195@gmail.com>
2019-01-09 11:31:15 +0000

    Facility tested and built.

--- src/main/java/Facility.java
+++ src/main/java/Facility.java
@@ -1,2 +1,19 @@
 public class Facility {
+
+    private int capacity;
+    private String name;
+
+    public Facility(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
 }
--- src/test/java/FacilityTest.java
+++ src/test/java/FacilityTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FacilityTest {
+
+    private Facility facility1;
+
+    @Before
+    public void before() {
+        facility1 = new Facility("Dining Room", 50);
+    }
+
+    @Test
+    public void facilityHasName() {
+        assertEquals("Dining Room", facility1.getName());
+    }
+
+    @Test
+    public void facilityHasCapacity() {
+        assertEquals(50, facility1.getCapacity());
+    }
+
 }
